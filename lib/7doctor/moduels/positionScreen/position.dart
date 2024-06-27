b843a5d498f8116be49859103369bbd90cb8ef1a
qusaijamous12 <qqjamous12@gmail.com>
2024-06-27 22:39:22 +0300

    Initial commit

---
+++ lib/7doctor/moduels/positionScreen/position.dart
@@ -0,0 +1,202 @@
+import 'package:doctorproject/7doctor/homescreen/homescreentwo.dart';
+import 'package:doctorproject/7doctor/moduels/bookingmodel/entity/userdata.dart';
+import 'package:doctorproject/7doctor/moduels/bookingmodel/firemanger.dart';
+import 'package:doctorproject/7doctor/moduels/paymentmodel/paymentscreen.dart';
+import 'package:doctorproject/models/usermodel.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class FillPositionPage extends StatefulWidget {
+  final LatLng initialPosition;
+
+  const FillPositionPage({super.key, required this.initialPosition});
+
+  @override
+  _FillPositionPageState createState() => _FillPositionPageState();
+}
+
+class _FillPositionPageState extends State<FillPositionPage> {
+  TextEditingController gender = TextEditingController();
+  TextEditingController name = TextEditingController();
+  var formKey=GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[800],
+        centerTitle: false,
+          systemOverlayStyle: SystemUiOverlayStyle(
+            statusBarIconBrightness: Brightness.light,
+            statusBarColor: Colors.blue[800]
+          ),
+          title: const Text('Fill Position'),
+
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: SingleChildScrollView(
+            child: Form(
+              key: formKey,
+              child: Column(
+
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image(
+                      image: AssetImage(
+                        'assets/images/map.png'
+                      )),
+                  SizedBox(height: 40),
+                  TextFormField(
+                    validator: (String ?value){
+                      if(value==null || value.isEmpty)
+                        {
+                          return 'You Have To Enter The Name';
+                        }
+                    },
+                    controller: name,
+                    decoration: InputDecoration(
+                        labelText: 'name',
+                      prefixIcon: Icon(
+                        Icons.drive_file_rename_outline
+                      ),
+                      border: OutlineInputBorder()
+                    ),
+                    keyboardType: TextInputType.text,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+
+                  TextFormField(
+                    validator: (String ?value){
+                      if(value==null || value.isEmpty)
+                      {
+                        return 'You Have To Enter The Gender';
+                      }
+                    },
+
+                    controller: gender,
+                    decoration: InputDecoration(
+                        labelText: 'gender',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(
+                        Icons.person
+                      )
+
+                    ),
+                    keyboardType: TextInputType.text,
+                  ),
+                  // TextFormField(
+                  //     controller: latitudeController,
+                  //     decoration: InputDecoration(labelText: 'Latitude'),
+                  //     keyboardType: TextInputType.number),
+                  // SizedBox(height: 10),
+                  // TextFormField(
+                  //   controller: longitudeController,
+                  //   decoration: InputDecoration(labelText: 'Longitude'),
+                  //   keyboardType: TextInputType.number,
+                  // ),
+                  SizedBox(height: 20),
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.blue[800],
+                      borderRadius: BorderRadiusDirectional.circular(20)
+                    ),
+                    child: MaterialButton(
+                        onPressed: (){
+                          if(formKey.currentState!.validate()){
+                            var data = AppointmentDataNerse(
+                              userId: FirebaseAuth.instance.currentUser?.uid,
+                              status: "initial",
+                              name: name.text,
+                              gender: gender.text,
+                              latitude: widget.initialPosition.latitude,
+                              longitude: widget.initialPosition.longitude,
+                              doctoruuid: "ygsiBi8mpQdQGLWtXu6Joa2w2mw1",
+                            );
+                            //Appointment.instance.sendPushNotificationTopic(usermodel!.token!);
+                            Appointment.instance.bookAppointmentnurse(data).then((value) {
+                              Fluttertoast.showToast(
+                                  msg: "Successfully",
+                                  toastLength: Toast.LENGTH_LONG,
+                                  gravity: ToastGravity.BOTTOM,
+                                  timeInSecForIosWeb: 1,
+                                  backgroundColor: Colors.blue[800],
+                                  textColor: Colors.white,
+                                  fontSize: 16.0
+                              );
+                              Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=>HomeScreenTwo()), (route) => false);
+                            }).catchError((error){
+                              Fluttertoast.showToast(
+                                  msg: "Failled",
+                                  toastLength: Toast.LENGTH_LONG,
+                                  gravity: ToastGravity.BOTTOM,
+                                  timeInSecForIosWeb: 1,
+                                  backgroundColor: Colors.blue[800],
+                                  textColor: Colors.white,
+                                  fontSize: 16.0
+                              );
+                            });
+
+                          }
+
+
+                        },
+                      child: Text(
+                        'Appointment',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20
+                        ),
+                      ),
+                        ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'You can choose to pay now or wait until you see the nurse to make your payment.',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 17
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Colors.blue[800],
+                        borderRadius: BorderRadiusDirectional.circular(20)
+                    ),
+                    child: MaterialButton(
+                      onPressed: (){
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=>MySample()));
+                        },
+                      child: Text(
+                        'Payment',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20
+                        ),
+                      ),
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
